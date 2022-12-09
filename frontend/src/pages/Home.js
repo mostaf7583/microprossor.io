88ad530c776a105d555c73985c20f4635c9c4cf7
mostaf7583 <73074293+mostaf7583@users.noreply.github.com>
2022-12-09 15:51:18 +0200

    second comment

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Home = () => {
+    return(
+        <div className="home">
+        <div>
+            <h2>Home</h2>
+        </div>
+        </div>
+        )
+   
+}
+
+export default Home
