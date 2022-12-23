026dabf6c6c316e790404dd43d0456dc2becb28f
mostaf7583 <73074293+mostaf7583@users.noreply.github.com>
2022-12-23 16:36:58 +0000

    klknk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
             <Routes>
               <Route
                 path="/"
-                element={<Home />}
+                element={<Home/>}
               />       
             </Routes>
           </div>
